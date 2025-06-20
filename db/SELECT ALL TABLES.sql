073b8de2ccd2a6b3d50cc1e51f332ad6ec4ebc3e
Ziqing12 <crellezq.han@gmail.com>
2025-06-19 22:43:08 -0400

    some fixes

--- db/SELECT ALL TABLES.sql
+++ db/SELECT ALL TABLES.sql
@@ -1,7 +1,29 @@
-SELECT * FROM Crime_Records;
-SELECT * FROM Victim;
-SELECT * FROM Times;
-SELECT * FROM Coordinates;
-SELECT * FROM Area;
-SELECT * FROM Weapon;
-SELECT * FROM Crime;
+USE crime_db;
+
+SELECT 
+    cr.dr_num,
+    cr.crime_code,
+    c.crime_desc,
+    cr.area_code,
+    a.area_name,
+    cr.weapon_code,
+    w.weapon_desc,
+    
+    t.date_reported,
+    t.date_occurred,
+    t.time_occurred,
+
+    v.age AS victim_age,
+    v.sex AS victim_sex,
+    v.race AS victim_race,
+
+    co.latitude,
+    co.longitude
+
+FROM Crime_Records cr
+LEFT JOIN Crime c ON cr.crime_code = c.crime_code
+LEFT JOIN Area a ON cr.area_code = a.area_code
+LEFT JOIN Weapon w ON cr.weapon_code = w.weapon_code
+LEFT JOIN Times t ON cr.dr_num = t.dr_num
+LEFT JOIN Victim v ON cr.dr_num = v.dr_num
+LEFT JOIN Coordinates co ON cr.dr_num = co.dr_num
