de0ecc3ae6da6c2c845246815be5d3197a573edc
Ziqing12 <crellezq.han@gmail.com>
2025-06-19 23:17:59 -0400

    updated select

--- db/Feature2--select crime by time.sql
+++ db/Feature2--select crime by time.sql
@@ -2,4 +2,4 @@ USE crime_db;
 
 SELECT dr_num, date_occurred
 FROM crime_records NATURAL JOIN times
-WHERE date_occurred >= '2020-01-01' and date_occurred <= '2020-12-01';
+WHERE date_occurred >= ? and date_occurred <= ?;
